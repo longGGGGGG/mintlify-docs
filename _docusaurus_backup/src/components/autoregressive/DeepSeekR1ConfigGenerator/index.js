729796e8e4d0c4da57e2a63d9742e296d37b6b28
zhoulong <553746008@qq.com>
2026-01-25 02:32:24 +0800

    upd sglang cookbook

--- _docusaurus_backup/src/components/autoregressive/DeepSeekR1ConfigGenerator/index.js
+++
@@ -1,8 +0,0 @@
-// DeepSeek R1 Configuration Generators
-// This module exports both basic and advanced (optimal) config generators
-
-export { default as DeepSeekR1BasicConfigGenerator } from './basic';
-export { default as DeepSeekR1AdvancedConfigGenerator } from './advanced';
-
-// Default export is the basic generator for backwards compatibility
-export { default } from './basic';
